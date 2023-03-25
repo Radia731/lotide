a855a27ffdf755969118be75c52e4547cd233ef7
Radia Ahmed <radiajjr17@gmail.com>
2023-03-25 01:51:35 -0400

    updated

--- assertEqual.js
+++ assertEqual.js
@@ -1,10 +1,7 @@
 const assertEqual = function(actual, expected) {
   if (actual === expected) {
-    console.log(`Assertion Passed: ${actual} === ${expected}`);
+    console.log(`✅ Assertion Passed: ${actual} === ${expected}`);
   } else {
-    console.log(`Assertion Failed: ${actual} !== ${expected}`);
+    console.log(`🛑 Assertion Failed: ${actual} !== ${expected}`);
   }
 };
-
-assertEqual("lighthouse Labs", "Bootcamp");
-assertEqual(1, 1);
