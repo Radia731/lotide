462f0007290742b46a8d46f6bf058c3554e5ba02
Radia Ahmed <radiajjr17@gmail.com>
2023-03-26 08:44:54 -0400

    update

--- head.js
+++ head.js
@@ -13,7 +13,7 @@ const head = function(array) {
   return array[0];
 };
 
-// Test cases
+
 assertEqual(head([5,6,7]), 5);
 assertEqual(head(["Hello", "Lighthouse", "Labs"]), "Hello");
 assertEqual(head([]), undefined);
