73f57e8f0764e68910ce85ff3f963e55368d9474
Radia Ahmed <radiajjr17@gmail.com>
2023-03-31 05:10:21 -0400

    countLettrs.js

---
+++ countLetters.js
@@ -0,0 +1,17 @@
+// const assertEqual = function(actual, expected) {
+const countLetters = function(str) {
+ const result = {};
+  str = str.replace(' ', '');
+    for ( const letter of str) {
+
+    if ( !result[letter])
+        result[letter] = 1;
+        else
+
+        result[letter]++
+    }
+return result;
+};
+
+console.log (countLetters('school stuff'));
+
