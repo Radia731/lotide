a6c9c270a56700243631168b5c0422a0ddf8cb1b
Radia Ahmed <radiajjr17@gmail.com>
2023-04-03 21:48:20 -0400

    modified assertArraysEqual

--- eqArray.js
+++ eqArray.js
@@ -19,8 +19,8 @@ const eqArrays = function(arr1, arr2) {
 };
 
 
-assertEqual(eqArrays([1, 2, 3], [1, 2, 3]), true); // should pass
-assertEqual(eqArrays([1, 2, 3], [3, 2, 1]), false); // should pass
-assertEqual(eqArrays(["1", "2", "3"], ["1", "2", "3"]), true); // should pass
-assertEqual(eqArrays(["1", "2", "3"], ["1", "2", 3]), false); // should pass
+assertEqual(eqArrays([1, 2, 3], [1, 2, 3]), true);
+assertEqual(eqArrays([1, 2, 3], [3, 2, 1]), false);
+assertEqual(eqArrays(["1", "2", "3"], ["1", "2", "3"]), true);
+assertEqual(eqArrays(["1", "2", "3"], ["1", "2", 3]), false);
 
