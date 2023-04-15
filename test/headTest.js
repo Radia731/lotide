de339d7d741c3b22a60a5474321e5189fb49ca22
Radia Ahmed <radiajjr17@gmail.com>
2023-04-15 17:28:15 -0400

    lotide

--- test/headTest.js
+++ test/headTest.js
@@ -5,19 +5,19 @@ const head   = index.head;
 
 
 describe("#head", () => {
-  // 1
+
   it("returns 1 for [1, 2, 3]", () => {
     assert.strictEqual(head([1, 2, 3]), 1);
   });
-  // 2
+
   it("returns '5' for ['5']", () => {
     assert.strictEqual(head(["5"]), "5");
   });
-  // 3
+
   it("returns '5' for ['5, 6, 7']", () => {
     assert.strictEqual(head([5, 6, 7]), 5);
   });
-  // 4
+  
   it("returns '1' for ['1, 6, 12']", () => {
     assert.strictEqual(head([1, 6, 12]), 1);
   });
--- test/middleTest.js
+++ test/middleTest.js
@@ -2,11 +2,11 @@
 const index = require('../index');
 const eqArrays = index.eqArrays;
 const assert = require('chai').assert;
-const middle   = index.middle;
+const middle = index.middle;
 
 describe("#middle", () => {
   it("returns [2,3] for [1, 2, 3, 4]", () => {
-    assert.strictEqual(eqArrays(middle([1, 2, 3, 4]), [2,3]), true);
+    assert.strictEqual(eqArrays(middle([1, 2, 3, 4]), [2, 3]), true);
   });
 
   it("returns [] for ['5']", () => {
