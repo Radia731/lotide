36b3c99227799773ccf9c1a2bf6430ae6d13f53e
Radia Ahmed <radiajjr17@gmail.com>
2023-04-16 18:34:15 -0400

    updated

--- index.js
+++ index.js
@@ -1,8 +1,31 @@
-const head = require('./head');
-const tail = require('./tail');
-const middle = require('./middle');
+const assertArraysEqual   = require('./assertArraysEqual');
+const assertEqual         = require('./assertEqual');
+const countOnly           = require('./countOnly');
+const eqArrays            = require('./eqArrays');
+const eqObjects           = require('./eqObjects');
+const findKey             = require('./findKey');
+const findKeybyValue      = require('./findKeybyValue');
+const head                = require('./head');
+const letterPositions     = require('./letterPositions');
+const map                 = require('./map');
+const middle              = require('./middle');
+const tail                = require('./tail');
+const takeUntil           = require('./takeUntil');
+const without             = require('./without');
+
 module.exports = {
-  head: head,
-  tail: tail,
-  middle: middle
+  assertArraysEqual,
+  assertEqual,
+  countOnly,
+  eqArrays,
+  eqObjects,
+  findKey,
+  findKeybyValue,
+  head,
+  letterPositions,
+  map,
+  middle,
+  tail,
+  takeUntil,
+  without
 };
--- middle.js
+++ middle.js
@@ -1,28 +1,18 @@
-const eqArrays = require('./eqArrays');
+const middle = function (inputArray) {
+  if (inputArray.length < 3) {
 
-const middleArray = function(arr1) {
-  // input: an array and a boolean
-  // output:  an array that include the number(s) in the middle of the input array, should be two numbers if arr1 is even, should be 1 number if arr1 is odd
-  // if length of arr1 is less than or equal to 2, the output should be an empty array
-  let outputArray = [];
-  let middleIndex = 0;
+    return [];
+  }
 
-  if (arr1.length <= 2) {
-    return outputArray;
+  if (inputArray.length % 2 === 0) {
+
+    const middleArr = Math.floor(inputArray.length / 2);
+    return inputArray.slice(middleArr - 1, middleArr + 1);
   } else {
-    // see if the input array is even or odd
-    if (arr1.length % 2 === 0) {
-      middleIndex = (arr1.length / 2) - 1;
-      outputArray.push(arr1[middleIndex]);
-      middleIndex = (arr1.length / 2);
-      outputArray.push(arr1[middleIndex]);
-      return outputArray;
-    } else {
-      middleIndex = Math.ceil((arr1.length / 2) - 1);
-      outputArray.push(arr1[middleIndex]);
-      return outputArray;
-    }
+
+    const middleArr = Math.floor(inputArray.length / 2);
+    return inputArray.slice(middleArr, middleArr + 1);
   }
 };
 
-module.exports = middleArray;
+module.exports = middle;
